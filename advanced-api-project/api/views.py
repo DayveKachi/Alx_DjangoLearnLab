9a7a31075bac6f4263b9876dfb21b9fd64480534
dayve <ukpaidavid@gmail.com>
2024-12-06 13:16:36 +0100

    Refactor imports in api views to contain filters.*

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django_filters import rest_framework
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters
 from .models import Book
 from .serializers import BookSerializer
 
@@ -10,7 +10,11 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    filter_backends = (SearchFilter, OrderingFilter, rest_framework.DjangoFilterBackend)
+    filter_backends = (
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        rest_framework.DjangoFilterBackend,
+    )
     filterset_fields = ("title", "author", "publication_year")
     search_fields = ("title", "author", "publication_year")
     ordering_fields = ("title", "publication_year")
