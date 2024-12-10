ee1e8341a61bdc4613e256edd51bc9d416bc480d
Dayve <ukpaidavid@gmail.com>
2024-12-10 23:09:00 +0100

    Stop tracking .env file

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Profile
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "published_date")
+    list_filter = ("author", "published_date")
+    search_fields = ("title", "author")
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "bio", "profile_picture")
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,8 +8,14 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
+    def __str__(self):
+        return self.title
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, default=None)
-    profile_picture = models.ImageField(upload_to="pictures/", blank=True, null=True)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to="profile_pics", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} profile"
