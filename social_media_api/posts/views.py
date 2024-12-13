23011caa5c662c372dbc20a248b85328737fb668
Dayve <ukpaidavid@gmail.com>
2024-12-13 04:30:40 +0100

    Finish Post and Comment Viewsets

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of a post or comment to edit or delete it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner of the post or comment.
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for performing CRUD operations on the Post model.
+    """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]  # Ensure users can only edit their own posts
+
+    def perform_create(self, serializer):
+        # Automatically set the author as the logged-in user
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for performing CRUD operations on the Comment model.
+    """
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]  # Ensure users can only edit their own comments
+
+    def perform_create(self, serializer):
+        # Automatically set the author as the logged-in user
+        post = serializer.validated_data.get("post")  # Ensure the post exists
+        if not post:
+            raise PermissionDenied("You must specify a post.")
+        serializer.save(author=self.request.user, post=post)
