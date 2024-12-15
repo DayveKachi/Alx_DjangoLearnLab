92344028d074756aca363df0e15fbf01e93f0fb6
Dayve <ukpaidavid@gmail.com>
2024-12-15 18:38:51 +0100

    Add more production settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -130,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Location for collectstatic output
+
+# Additional static files directories
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Custom static files directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
